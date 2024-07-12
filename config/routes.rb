ec56e118feee3cac22eb7aae7db7837f28658522
Paul McKissock <paulsmckissock@gmail.com>
2024-07-12 15:22:20 -0400

    [#4] Removed unnecessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
     member do
       post :temp_create
       post :temp_delete
-      patch :save
     end
   end
-
-  get "/wheels/:id", to: "wheels#show"
-  get "/wheels/:id/edit", to: "wheels#edit"
 end
