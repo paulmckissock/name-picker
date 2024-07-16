79956c8fa27683597276135f6a87be0ce911888e
Ciel Park <117504912+cielcp@users.noreply.github.com>
2024-07-16 17:36:11 -0400

    Signup Styling (#23)
    * Install Tailwind Gem
    
    * Tailwind Gem Testing
    
    Not sure why only the spacing utilities are working? Things like background color and text color aren't changing for me
    
    * Successfully Installed
    
    * [#10] Signup Base Styling
    
    * [#10] Styling Login to Match
    
    * [#10] PR Fixes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,32 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          300: "#4BB0E3",
+          800: "#1F559E",
+        },
+        orange: "#E66C15",
+        yellow: "#EEB71E",
+
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
