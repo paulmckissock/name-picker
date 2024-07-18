f97c15adb6bc768a0ff97885269415c51477837f
Paul McKissock <paulsmckissock@gmail.com>
2024-07-17 21:50:56 -0400

    [#N/A] Fixed seeds.rb and changed authenticate_user! to redirect to signup path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     wheels_path
   end
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_registration_path, alert: 'You need to sign in or sign up before continuing.'
+    end
+  end
 end
