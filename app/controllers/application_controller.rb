f8a90ececa7c2b69889393f7de8ea109ef507bc4
Paul McKissock <paulsmckissock@gmail.com>
2024-07-17 22:13:44 -0400

    [#N/A] Reverted authenticate_user! changes and changed heading of wheel page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
     wheels_path
   end
 
-  protected
+  # protected
 
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_registration_path, alert: "You need to sign in or sign up before continuing."
-    end
-  end
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   else
+  #     redirect_to new_user_registration_path, alert: "You need to sign in or sign up before continuing."
+  #   end
+  # end
 end
