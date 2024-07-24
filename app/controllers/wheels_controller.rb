16646f4837a3d2196d1eaacffa66661efcfdc27c
Paul McKissock <paulsmckissock@gmail.com>
2024-07-24 11:04:48 -0400

    [N/A] Standardrb --fix and made check_unsaved_changes much more simple.

--- app/controllers/wheels_controller.rb
+++ app/controllers/wheels_controller.rb
@@ -84,12 +84,7 @@ class WheelsController < ApplicationController
 
   def check_unsaved_changes
     load_temp_participants
-    has_unsaved_changes = unsaved_changes?
-    render json: { unsaved_changes: has_unsaved_changes }
-  end
-
-  def unsaved_changes?
-    temp_participants != wheel.participants.pluck(:name)
+    render json: {unsaved_changes: temp_participants != wheel.participants.pluck(:name)}
   end
 
   private
